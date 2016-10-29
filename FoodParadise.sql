020e6b35d3cd4cbd377709d3b72b2122b6833740
aanchal <aanchal.g.gupta@gmail.com>
2016-10-29 11:11:59 -0400

    sql

---
+++ FoodParadise.sql
@@ -0,0 +1,9 @@
+CREATE database FoodParadise;
+USE  FoodParadise;
+CREATE TABLE FoodReservation1(
+  CustomerName VARCHAR(100),
+  UserPhone INTEGER(30),
+  CustomerEmail VARCHAR(150),
+  CustomerID INTEGER AUTO_INCREMENT PRIMARY KEY
+  );
+
